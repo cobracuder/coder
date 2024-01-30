6c1332f5e932937fe04ed77a713c9b42bca34111
anirudh <lookingforanirudh@gmail.com>
2024-01-30 17:31:08 +0530

    first commit

---
+++ factorial.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int factorial(int val)
+{
+    if(val==1 or val ==0)
+    return 1;
+    return val*factorial(val-1);
+}
+int main()
+{
+    cout<<"\n Enter the number for factorial: ";
+    int num;
+    cin>>num;
+    cout<<factorial(num);
+}
+
